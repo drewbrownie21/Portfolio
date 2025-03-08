ba3bd93237745bb961f708f3b6ad7cdd5c5aab9e
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-07 23:37:22 -0800

    Add an education component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import Education from './components/Education';
 
 function App(){
 
   return(
-    <div
-    className='main-header'
-    >
-      Drew's Portfolio
+    <div>
+      <div className='main-header'>
+        Drew's Portfolio
+      </div>
+      <h2>
+        <strong>
+          Education
+        </strong>
+        <Education />
+      </h2>
+      
     </div>
   )
 }
---
+++ src/components/Education.tsx
@@ -0,0 +1,19 @@
+
+function Education(){
+
+    return(
+        <div className='university-information'>
+          <h3>
+            University of Nevada, Reno
+          </h3>
+          <p>
+            Bachelor of Science in Electrical Engineering
+          </p>
+          <p>
+            Minors in Unmanned Autonomous Systems and Mathematics
+          </p>
+        </div>
+    )
+}
+
+export default Education;
