9927816f860c12cdf109f4f307c9cae81587ea73
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-10 22:40:29 -0700

    Add a github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Education from "./components/Education";
 import myHeadshot from "./assets/drew.png";
+import githubIcon from "./assets/github-mark.svg";
 import Experience from "./components/Experience";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="bg-neutral-100 min-h-screen text-black font-mono">
+      <h1>
+        <a href="https://github.com/drewbrownie21">
+          <img className="w-10 h-10" src={githubIcon} />
+        </a>
+      </h1>
       <div className="flex items-center justify-center space-x-10 p-30">
         <Header />
         <img className="w-100 h-100 rounded-full" src={myHeadshot} />
