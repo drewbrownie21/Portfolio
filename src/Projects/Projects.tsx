2b5d33cf4d5ab28a668a2e89d03d19c8940b5032
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-19 19:29:23 -0700

    Updates

--- src/Projects/Projects.tsx
+++
@@ -1,9 +0,0 @@
-function Projects() {
-  return (
-    <div className="flex items-center justify-center py-30">
-      Projects! Under Construction...
-    </div>
-  );
-}
-
-export default Projects;
