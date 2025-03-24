c6007553436040c8cba56958b9e052e4c822d7ac
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-24 14:33:07 -0700

    small updates

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,11 +1,11 @@
 import githubIcon from "../assets/github-mark.svg";
 import linkedin from "../assets/linkedin.png";
-import Projects from "./ProjectsButton";
+import ProjectsButton from "./ProjectsButton";
 
 function Header() {
   return (
     <div className="flex items-center justify-end py-2 px-2">
-      <Projects />
+      <ProjectsButton />
       <div>
         <a href="https://github.com/drewbrownie21">
           <img className="w-10 h-10" src={githubIcon} />
--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -4,7 +4,7 @@ function Title() {
   return (
     <div className="flex items-center justify-center space-x-10 p-20">
       <div className="text-xl font-medium">
-        Drew Brown, Frontend Web Developer
+        Drew Brown, Frontend Developer
       </div>
       <img className="w-full h-auto max-w-[150px] sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] xl:max-w-[350px] rounded-full" src={myHeadshot} />
     </div>
