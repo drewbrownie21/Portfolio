385d9036be0fc4388377a8b4883a66ef42c582ff
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-30 14:32:33 -0700

    Add shadow to image

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -2,13 +2,13 @@ import myHeadshot from "../assets/drew.png";
 
 function Title() {
   return (
-    <div className="flex items-center justify-center space-x-10 p-20">
+    <div className="flex items-center justify-center space-x-10 p-20 overflow-visible">
       <div>
         <p className="text-4xl font-bold">Drew Brown </p>
         <p>Frontend Developer </p>
       </div>
       <img
-        className="w-full h-auto max-w-[150px] sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] xl:max-w-[350px] rounded-full"
+        className="w-full h-auto max-w-[150px] sm:max-w-[200px] md:max-w-[250px] lg:max-w-[300px] xl:max-w-[350px] rounded-full shadow-2xl bg-white"
         src={myHeadshot}
       />
     </div>
