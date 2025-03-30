3e1b74b501ac74aac11e7fe07acc3c8b8c80e3b3
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-03-30 14:24:35 -0700

    Added multiple jobs do iterate through

--- src/Background/Experience.tsx
+++ src/Background/Experience.tsx
@@ -1,35 +1,53 @@
 import { useState } from "react";
 
-const jobs = [
+type Jobs = {
+  company: string;
+  jobTitle: string;
+  dateRange: string;
+  duties: String[];
+};
+
+const jobs: Jobs[] = [
   {
-    company : "Ridgeline",
+    company: "Ridgeline",
     jobTitle: "Quality Engineer II",
+    dateRange: "2024 - Current",
     duties: [
-      "Write automation tests with Cypress and Karate",
-      "Triage daily automation run failures",
-      "Fix minor bugs on the frontend and help maintain code base"
-    ]
+      "Write automation tests with Cypress and Karate.",
+      "Triage daily automation run failures.",
+      "Fix minor bugs on the frontend and help maintain code base.",
+    ],
   },
   {
-    company : "School Pathways",
+    company: "School Pathways",
     jobTitle: "Software Test Engineer",
+    dateRange: "2021 - 2024",
     duties: [
-      "Created automated test suite utilizing Cypress and Selenium",
-      "Wrote API-level tests using Python and pytest",
-      "Reduced deploy test time 86%, improving efficiency and accelerating the release cycle through automated testing."
-    ]
-  }
-]
+      "Created automated test suite utilizing Cypress and Selenium.",
+      "Wrote API-level tests using Python and pytest.",
+      "Reduced deploy test time 86%, improving efficiency and accelerating the release cycle through automated testing.",
+    ],
+  },
+  {
+    company: "Scientifc Games",
+    jobTitle: "Software Quality Assuance Engineer",
+    dateRange: "2019 - 2021",
+    duties: [
+      "Tested games to ensure the final product was compliant and ready for field use.",
+      "Developed and maintained the test process to have a comprehensive test plan.",
+      "Automated and optimized the compliance submission process using Python scripts.",
+    ],
+  },
+];
 
 function Experience() {
-  const [selection, setSelection] = useState(0)
+  const [selection, setSelection] = useState(0);
 
-  function handleCount(countInt: number){
-    console.log(selection)
-    if(selection == jobs.length - 1){
-      setSelection(0)
-    }else{
-      setSelection(selection + countInt)
+  function handleCount(countInt: number) {
+    if (selection == jobs.length - 1) {
+      setSelection(0);
+    } else {
+      setSelection(selection + countInt);
     }
   }
 
@@ -42,18 +60,22 @@ function Experience() {
         Experience
       </h1>
       <div className="leading-relaxed">
-        <h1>{jobs[selection].company}</h1>
+        <h2 className="flex flex-row justify-between items-center mx-auto">
+          <div className="text-left">{jobs[selection].company}</div>
+          <div className="text-right">{jobs[selection].dateRange}</div>
+        </h2>
         <div>
-          <p className="text-lg font-semibold">Quality Engineer II</p>
-          • Write automation tests with Cypress and Karate <br />
-          • Triage daily automation run failures <br />• Fix minor bugs on the
-          frontend and help maintain code base
+          <p className="text-lg font-semibold">{jobs[selection].jobTitle}</p>
         </div>
-        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 active:bg-gray-500"
+        <ul>
+          {jobs[selection].duties.map((item, index) => (
+            <li key={index}>• {item}</li>
+          ))}
+        </ul>
+        <button
+          className="w-8 h-8 flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 active:bg-gray-500"
           onClick={() => handleCount(1)}
-        >
-  {/* You can place an icon inside, like a left or right arrow */}
-</button>
+        ></button>
       </div>
     </div>
   );
