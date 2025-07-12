d9a33616c6a43273803d4010b5a5d2009ae71cb7
drewbrownie21 <36270973+drewbrownie21@users.noreply.github.com>
2025-07-11 22:25:10 -0700

    Update Skills.tsx

--- src/Background/Skills.tsx
+++ src/Background/Skills.tsx
@@ -1,6 +1,6 @@
 function Skills() {
   return (
-<section className="flex-1 w-full bg-blue-100 text-blue-900 rounded-2xl shadow-md p-6 overflow-hidden">
+    <section className="flex-1 w-full bg-blue-100 text-blue-900 rounded-2xl shadow-md p-6 overflow-hidden">
       <h1 className="font-bold text-xl text-center border-b-2 border-blue-300 pb-2">
         Skills & Technologies
       </h1>
